70b2b77296cd1b3c76bba6c4d0020e9bbe0c9379
et3nalblade <anemokapibara@yandex.ru>
2024-12-25 13:11:55 +0300

    дописана составляющая файла game.py

--- game.py
+++ game.py
@@ -1,33 +1,30 @@
 import pygame
-from scripts import *
+
 from __init__ import *
-# Инициализация игры
 pygame.init()
 
-# Настройки экрана
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN if FULLSCREEN else 0)
 
-# Основной игровой цикл
 def main():
     clock = pygame.time.Clock()
 
-    # Создаем главное меню
+
     main_menu = MainMenu()
 
-    # Игровой цикл
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Запускаем главное меню
+
         main_menu.display(DISPLAYSURF)
 
-        # Обновляем экран
+
         pygame.display.update()
 
-        # Поддерживаем 120 FPS
+
         clock.tick(FPS)
 
     pygame.quit()
